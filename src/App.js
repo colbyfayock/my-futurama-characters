20d4e42aaf69e214b63e684e012cd2f8c95d427b
Colby Fayock <fayock@gmail.com>
2020-05-24 12:24:39 -0400

    Step 0: prepping app with content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import characters from './characters.json';
 
 function App() {
   return (
-    <div className="App">
+    <>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <h1>Futurama Characters</h1>
+        </div>
       </header>
-    </div>
+
+      <main className="container">
+        <ul className="characters">
+          {characters.map(character => {
+            const { name, company, species, thumb } = character;
+            return (
+              <li key={name} className="character">
+                <span className="character-thumb" style={{
+                  backgroundImage: `url(${thumb})`
+                }} />
+                <ul className="character-meta">
+                  <li>
+                    <strong>Name:</strong> { name }
+                  </li>
+                  <li>
+                    <strong>Company:</strong> { company }
+                  </li>
+                  <li>
+                    <strong>Species:</strong> { species }
+                  </li>
+                </ul>
+              </li>
+            )
+          })}
+        </ul>
+        <aside>
+          <form className="search">
+            <label>Search</label>
+            <input type="text" />
+          </form>
+        </aside>
+      </main>
+
+      <footer>
+        <div className="container">
+          <p>
+            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a> via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
+          </p>
+        </div>
+      </footer>
+    </>
   );
 }
 
